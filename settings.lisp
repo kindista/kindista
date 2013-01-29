2e1a67ab35010f32320f2caadc0c34bc03c3dd2c
Benjamin Crandall <ben@commongoods.net>
2013-01-29 14:27:11 -0800

    .

--- settings.lisp
+++
@@ -1,8 +0,0 @@
-(in-package :kindista)
-
-(define-constant +db-path+ "/home/walker/k2/data/" :test #'string=)
-(define-constant +avatar-path+ "/home/walker/k2/media/avatars/"; <- trailing slash
-                 :test #'string=)
-(define-constant +mail-server+ "localhost" :test #'string=)
-
-
