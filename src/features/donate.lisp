888203774b5cc2460462029035ba72321bd87598
Nicholas E. Walker <root@subtlepath.com>
2013-05-01 13:37:07 -0700

    use parameter for stripe publishable key

--- src/features/donate.lisp
+++ src/features/donate.lisp
@@ -198,7 +198,7 @@
                    (setf *processing* t)
                    (dolist (element ((@ document get-elements-by-tag-name) "label"))
                      ((@ element set-attribute) "class" ""))
-                   ((@ *stripe set-publishable-key) "pk_test_ffSdkWiT4zyspRyxsM8tb0tI")
+                   ((@ *stripe set-publishable-key) *stripe-publish-key*)
                    ((@ *stripe create-token)
                     (create
                       :number (@ ((@ document get-element-by-id) "ccn") value)
