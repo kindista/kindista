64165ef2baabd9e9bee566652d3a698d2bcbd77c
Benjamin Crandall <ben@commongoods.net>
2013-05-20 14:51:32 -0700

    sharing guidelines open in a new tab

--- src/shared/inventory.lisp
+++ src/shared/inventory.lisp
@@ -429,7 +429,9 @@
           (:p
             (:strong "Important: ") 
             "Please read the "
-            (:a :href "/faq#sharing-guidelines" "Guidelines for Sharing on Kindista")
+            (:a :href "/faq#sharing-guidelines" 
+                :target "_blank"
+                "Guidelines for Sharing on Kindista")
             " before posting any offers or requests. "
             "All offers and requests on Kindista are subject to these guidelines.")
 
