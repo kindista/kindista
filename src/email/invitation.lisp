b77312223c15f4dbce50ff40bc514da15eea52ca
Benjamin Crandall <ben@commongoods.net>
2013-05-31 12:42:02 -0700

    back to old admin invites

--- src/email/invitation.lisp
+++ src/email/invitation.lisp
@@ -137,92 +137,6 @@ sender
                                                                    :text text))))
 
 (defun requested-invite-email-text (token name email &key text)
-(s+ "Greetings,
-"
-"
-At some point last year, likely at a talk given by Charles Eisenstein, you signed up to be notified when Kindista.org was available in your area.  Although it has taken longer than we had hoped, we are excited to invite you to join our gift-economy network!
-"
-
-"
-You may be one of the first people to sign up in your local area. If that is the case, please start by inviting your friends to join and please feel free to contact us for suggestions about how to jump start a new sharing network in your local community."
-
-"
-
-Your invitation code is " (write-to-string token) ".
-This invitation will expire in 60 days.
-
-"
-
-(when text
-(s+ "Personal message from the Kindista crew: 
-\""
-text "\"
-
-"))
-
-"We are excited for you to join the Kindista community!
-
-"
-
-"Please click on this link or copy and paste it into your browser"
-" to create your account:
-"
-(url-compose (s+ +base-url+ "signup")
-             "email" email
-             "token" token)
-
-"
-Thanks for sharing your gifts with us!
-"
-"-The Kindista Team"))
-
-
-(defun requested-invite-email-html (token name email &key text)
-(html-email-base
-  (html
-    (:p :style *style-p*
-     "Greetings,")
-    
-    (:p :style *style-p*
-"At some point last year, likely at a talk given by Charles Eisenstein, you signed up to be notified when Kindista.org was available in your area.  Although it has taken longer than we had hoped, we are excited to invite you to join our gift-economy network!")
-
-    (:p :style *style-p*
-     " You may be one of the first people to sign up in your local area. If that is the case, please start by inviting your friends to join and please feel free to contact us for suggestions about how to jump start a new sharing network in your local community. ")
-     
-    (:p :style *style-p*
-     "Your invitation code is " (:strong (str (write-to-string token)) ".")
-     (:br)
-     "This invitation will expire in 60 days.")
-
-    (when text
-      (htm (:table :cellspacing 0
-                   :cellpadding 0
-                   :style *style-quote-box*
-             (:tr (:td :style "padding: 4px 12px;"
-                     "Personal message from the Kindista crew: "
-                      (:br)
-                      "\"" (str text) "\"")))))
-
-    (:p :style *style-p*
-      "We are excited for you to join the Kindista community!")
-
-    (:p :style *style-p*
-      "Please click on this link or copy and paste it into your browser"
-      " to create your account: "
-      (:br)
-      (:a :href (url-compose (s+ +base-url+ "signup")
-                             "email" email
-                             "token" token)
-                (str (url-compose (s+ +base-url+ "signup")
-                                  "email" email
-                                  "token" token))))
-
-    (:p :style *style-p*
-      "Thanks for sharing your gifts with us!")
-
-    (:p "-The Kindista Team"))))
-#|
-(defun requested-invite-email-text (token name email &key text)
 (s+ "Hi " name ",
 "
 "
@@ -297,4 +211,3 @@ Thanks for sharing your gifts with us!
       "Thanks for sharing your gifts with us!")
 
     (:p "-The Kindista Team"))))
-|#
