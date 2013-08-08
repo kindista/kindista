d2a0da1ac8aba7e730947a23def0323c7913f614
Nicholas E. Walker <root@subtlepath.com>
2013-08-07 17:02:07 -0700

    make side menu thumbnail a PNG

--- src/http/main.lisp
+++ src/http/main.lisp
@@ -459,7 +459,7 @@
                          (:table
                            (:tr
                              (:td :rowspan "2"
-                               (:a :href link (:img :src (get-avatar-thumbnail *userid* 100 100))))
+                               (:a :href link (:img :src (get-avatar-thumbnail *userid* 100 100 :filetype "png"))))
                              (:td (:a :href link (str (getf *user* :name)))))
                            (:tr
                              (:td
