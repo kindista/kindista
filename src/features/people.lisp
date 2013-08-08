6c753d1abc5ef5f2ef552f72f52f9b6992108d78
Benjamin Crandall <ben@commongoods.net>
2013-08-07 17:14:32 -0700

    fixed a bug in profile bio section

--- src/features/people.lisp
+++ src/features/people.lisp
@@ -314,8 +314,9 @@
           (editing
             (htm
               (:form :method "post" :action "/settings"
-                (:input :type "hidden" :name "next" :value *base-url*)
-                (:textarea :name (strcat "bio-" section-name) (str content))
+                (:input :type "hidden" :name "next" :value (strcat *base-url* "/about"))
+                (:textarea :name (strcat "bio-" section-name)
+                           (str (escape-for-html content)))
                 (:button :class "yes" :type "submit" :name "save" "Save")
                 (:a :class "cancel" :href *base-url* "Cancel")
                 )))
@@ -325,7 +326,7 @@
 
           (t
             (htm
-              (:p (str content)))))))))
+              (:p (str (html-text content))))))))))
 
 (defun profile-top-html (userid)
   (let ((user (db userid))
@@ -414,7 +415,6 @@
                        :editable editable))))
             (htm (:h3 "This person hasn't written anything here."))))
         :right (if editing
-                 (html (:h2 "Style guide"))
                  (when mutuals
                    (mutual-connections-sidebar mutual-links)))
         :top (profile-top-html userid)
@@ -511,17 +511,6 @@
        ;   (profile-bio-html id)
        ;   (profile-activity-html id)))
 
-        ((or (not (eql id *userid*))
-             (getf *user* :bio-summary)
-             (getf *user* :bio-into)
-             (getf *user* :bio-contact)
-             (getf *user* :bio-skils)
-             (getf *user* :bio-doing))
-         (profile-activity-html id))
-
-        ((not editing)
-         (profile-bio-html id))
-
         ((string= editing "doing")
          (profile-bio-html id :editing 'doing))
 
@@ -536,6 +525,14 @@
 
         ((string= editing "skills")
          (profile-bio-html id :editing 'skills))
+
+        ((or (not (eql id *userid*))
+             (getf *user* :bio-summary)
+             (getf *user* :bio-into)
+             (getf *user* :bio-contact)
+             (getf *user* :bio-skils)
+             (getf *user* :bio-doing))
+         (profile-activity-html id))
 
         (t (not-found))))))
 
