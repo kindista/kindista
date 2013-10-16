35e3b3dd0abec4d4280d2d2bfdc4fa729f348064
Benjamin Crandall <ben@commongoods.net>
2013-10-16 16:56:11 -0700

    make the bot-scanner catch more bots

--- src/helpers.lisp
+++ src/helpers.lisp
@@ -19,7 +19,8 @@
 
 (defparameter +number-scanner+ (create-scanner "^\\d+$"))
 (defparameter +full-name-scanner+ (create-scanner "^([a-zA-Z]+\\.? )[a-zA-Z]+"))
-(defparameter +bot-scanner+ (create-scanner "(/bot.html)|(/spider.html)|(.com/robot)"))
+(defparameter +bot-scanner+ (create-scanner "(spider)|(bot)" :case-insensitive-mode t))
+
 (defparameter +text-scanner+ (create-scanner "[a-zA-Z]+"))
 (defparameter +email-scanner+ (create-scanner
                                  "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$"))
