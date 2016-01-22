2695c6567f5b001bbd12fbb8cb0d6c0ae59d913a
ArianLeffingwell <arianleffingwell@gmail.com>
2016-01-21 17:32:34 -0800

    ommited fetch event listener for cached pages

--- html/service-worker.js
+++ html/service-worker.js
@@ -15,17 +15,3 @@ self.addEventListener('install', function(event) {
         })
     );
 });
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
