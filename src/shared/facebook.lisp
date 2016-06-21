67770ad667fda06d4c387fc82579afcdefb64c9e
Benjamin Crandall <ben@commongoods.net>
2016-06-21 11:39:33 -0700

    debugging open graph metatags

--- src/shared/facebook.lisp
+++ src/shared/facebook.lisp
@@ -69,8 +69,6 @@
       (htm
         (:meta :property "og:type"
                :content (s+ "kindistadotorg:" typestring))))
-    (:meta :property "site_name"
-           :content "Kindista")
     (awhen determiner
       (htm (:meta :property "og:determiner" :content it)))
     (:meta :property "fb:app_id"
